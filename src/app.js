03f7fb568921fd032745620602573a70adedd82e
andre <gombkaa@gmail.com>
2021-06-25 21:39:03 +0200

    Install mongoose and dotenv package

--- src/app.js
+++ src/app.js
@@ -1,9 +1,18 @@
 const express = require("express");
-
 const app = express();
+const mongoose = require("mongoose");
+require("dotenv/config");
+
+const workoutRoute = require("./routes/workouts");
+
+app.use("/workouts", workoutRoute);
 
 app.get("/", (req, res) => {
-  res.send("Hello there!");
+  res.send("We are on home route");
 });
+
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () =>
+  console.log("connected to DB")
+);
 
 app.listen(3000);
