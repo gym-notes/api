a73736f27c72615bfe2640a982d1a34082623f62
andre <gombkaa@gmail.com>
2021-08-14 02:28:02 +0200

    Add shity registration and login

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,22 @@
 import jwt from "jsonwebtoken";
 import "dotenv/config";
+import bcrypt from "bcryptjs";
+import user from "../models/User.js";
 
 export default {
-  login(req, res) {
+  async login(req, res) {
     const { email, password } = req.body;
 
-    if (email === "user@example.com" && password === "example") {
+    const foundUser = await user.findOne({
+      email: email,
+    });
+
+    if (!foundUser)
+      res.status(400).json({ message: "wrong email or password" });
+
+    const correctPassword = await bcrypt.compare(password, foundUser.password);
+
+    if (correctPassword) {
       const secret = process.env.ACCESS_TOKEN_SECRET;
       const lifetime = parseInt(process.env.ACCESS_TOKEN_LIFETIME_IN_SECONDS);
 
@@ -14,10 +25,27 @@ export default {
       return res.status(200).json({ token });
     }
 
-    return res.status(401).json({ message: "Auth Failed" });
+    return res.status(401).json({ message: "wrong email or password" });
   },
 
-  register(req, res) {
-    res.status(200).json({ message: "authenticated route" });
+  async register(req, res) {
+    const { email, password } = req.body;
+
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    try {
+      await user.create({
+        email,
+        password: passwordHash,
+      });
+    } catch (error) {
+      if (error.code === 11000) {
+        res.status(400).json({ message: "email already taken" });
+      } else {
+        res.status(400).json({ message: "something went wrong" });
+      }
+    }
+
+    res.status(201).end();
   },
 };
--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,9 @@
-import authentication from "../middleware/authentication.js";
 import { Router } from "express";
 import authController from "../controllers/authController.js";
 
 const router = Router();
 
 router.post("/login", authController.login);
-router.post("/register", authentication.authenticate, authController.register);
+router.post("/register", authController.register);
 
 export default router;
