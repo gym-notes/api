9e36ed2d38456ecbed64560fca92831a954684a7
andrzejwojciak <gombkaa@gmail.com>
2021-08-16 17:17:25 +0200

    Clean authentication middleware module

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,18 +1,18 @@
 import jwt from "jsonwebtoken";
 import "dotenv/config";
 
-export default {
-  authenticate(req, res, next) {
-    const authHeader = req.headers["authorization"];
-    const token = authHeader && authHeader.split(" ")[1];
+function authenticate(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
 
-    if (!token) return res.status(401).json({ message: "unauthenticated" });
+  if (!token) return res.status(401).json({ message: "unauthorized" });
 
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-      if (err) return res.status(401).json({ message: err });
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.status(401).json({ message: err });
 
-      req.user = user;
-      next();
-    });
-  },
-};
+    req.user = user;
+    next();
+  });
+}
+
+export default authenticate;
