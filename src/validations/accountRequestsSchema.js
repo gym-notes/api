264b302a334e25a5a41ca75bf8acaeca772a8a2b
Andrzej <gombkaa@gmail.com>
2021-12-17 22:31:39 +0100

    Fix update account schema validation

--- src/validations/accountRequestsSchema.js
+++ src/validations/accountRequestsSchema.js
@@ -1,12 +1,12 @@
 import Joi from 'joi';
 
-const schema = Joi.object({
+const updateAccountSchema = Joi.object({
   firstName: Joi.string().required(),
   weight: Joi.number().min(1).required(),
   height: Joi.number().min(1).required(),
   birthDate: Joi.date().required(),
-  gender: Joi.string().required(),
+  gender: Joi.string().valid('male', 'female').required(),
   country: Joi.string().required(),
 });
 
-export default schema;
+export default updateAccountSchema;
